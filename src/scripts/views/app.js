55a9acab8768c651f73555d692cc3dfd69893471
Aulia Salsa <Asus@Akbars-MacBook-Pro.local>
2023-12-22 09:43:53 +0700

    First Commit

---
+++ src/scripts/views/app.js
@@ -0,0 +1,31 @@
+import DrawerInitiator from "../utils/drawer-initiator";
+
+class App {
+    constructor({button, drawer, content}){
+        this._button = button;
+        this._drawer = drawer;
+        this._content = content;
+        this._initialAppShell();
+    }
+
+    _initialAppShell(){
+        //adalah sebuah method yang bertujuan untuk menginisiasikan komponen appShell yang sudah dimasukkan pd properti class
+        DrawerInitiator.init({
+            button: this._button,
+            drawer: this._button,
+            content: this._content,
+        });
+    }
+
+}
+
+export default App;
+
+//kelas App bertujuan menginisiasikan komponen-komponen App Shell, ketika web dimuat, AppShell yang pertama dijalankan adalah kelas App
+//komponen AppShell yang di inisiasikan adalah komponen pembentuk fitur drawer
+//hamburgerButton => untuk menampilkan drawer content
+//drawerContent => untuk menampung list navigasi
+//mainContent => untuk menyembunyikan drawerContent ketika di klik
+//buat constructor App dengan 3 parameter => button, drawer, dan content
+
+//Karena parameter lebih dari 2 tidak direkomendasikan oleh clean code, maka buat parameternya menjadi objek destructuring ES6
